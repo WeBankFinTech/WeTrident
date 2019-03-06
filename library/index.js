df0163bad9d4aaed501e66b72e188f5ad4001324
erichua <erichua23@gmail.com>
2019-03-06 19:51:46 +0800

    rename

---
+++ library/index.js
@@ -0,0 +1,8 @@
+import TridentApp from './TridentApp'
+import AppNavigator from './navigation/AppNavigator'
+import * as reduxUtils from './reduxUtils'
+export default {
+  ...reduxUtils,
+  TridentApp,
+  AppNavigator
+}
--- trident-core/index.js
+++
@@ -1,8 +0,0 @@
-import TridentApp from './library/TridentApp'
-import AppNavigator from './library/navigation/AppNavigator'
-import * as reduxUtils from './library/reduxUtils'
-export default {
-  ...reduxUtils,
-  TridentApp,
-  AppNavigator
-}
