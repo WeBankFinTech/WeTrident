cf738bee697c9688465435bb849bf2ba516190bb
erichua <erichua23@gmail.com>
2019-05-07 20:06:30 +0800

    fix library export

--- library/index.js
+++ library/index.js
@@ -1,10 +1,12 @@
 import TridentApp from './TridentApp'
 import AppNavigator from './navigation/AppNavigator'
 import * as reduxUtils from './reduxUtils'
+
 export default {
-  ...reduxUtils,
-  AppNavigator
+  ...reduxUtils
 }
+
 export {
+  AppNavigator,
   TridentApp
 }
