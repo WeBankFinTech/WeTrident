9fb22bd7717cefd53cb6861ca9ff7a62f8ed7ba4
erichua <erichua23@gmail.com>
2019-05-08 15:26:29 +0800

    remove code template in scripts

--- app-seed/scripts/gulp/codeTemplate/moduleTpl/manifest.js
+++
@@ -1,7 +0,0 @@
-/**
- * 记录所有Scene的列表
- *
- * Created by {{author}} on {{createAt}}.
- */
-
-export default []
--- app-seed/scripts/gulp/codeTemplate/sceneTpl/constants/ReportKeys.js
+++
@@ -1,9 +0,0 @@
-/**
- * 每个Scene的上报KEY分别放在Scene的constants下面
- * 上报全都走ReportKey常量，并且请标明上报的用途和key的含义
- *
- * Created by {{author}} on {{createAt}}.
- */
-export default Object.freeze({
-
-})
