547c38fd9029e70cfb4d975305ba330decb00bd0
erichua <erichua23@gmail.com>
2019-06-12 18:12:02 +0800

    增加Trident docs website

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -5,14 +5,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require("react");
+const React = require('react');
 
-const CompLibrary = require("../../core/CompLibrary.js");
+const CompLibrary = require('../../core/CompLibrary.js');
 
-const {MarkdownBlock, GridBlock, Container} = CompLibrary; /* Used to read markdown */
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
 
 const siteConfig = require(`${process.cwd()}/siteConfig.js`);
-
 function docUrl(doc, language) {
   return `${siteConfig.baseUrl}${language ? `${language}/` : ""}${doc}`;
 }
@@ -21,59 +22,65 @@ function imgUrl(img) {
   return `${siteConfig.baseUrl}img/${img}`;
 }
 
-class Button extends React.Component {
-  render() {
-    return (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button hero" href={this.props.href} target={this.props.target}>
-          {this.props.children}
-        </a>
-      </div>
-    );
-  }
-}
-
-Button.defaultProps = {
-  target: "_self"
-};
-
-const SplashContainer = props => (
-  <div className="homeContainer">
-    <div className="homeSplashFade">
-      <div className="wrapper homeWrapper">{props.children}</div>
-    </div>
-  </div>
-);
-
-
-const ProjectTitle = () => (
-  <React.Fragment>
-    <div style={{display : "flex", justifyContent : "center", alignItems : "center"}}>
-      {/*<img src={"img/redux.svg"} alt="Redux logo" width={100} height={100}/>*/}
-      <h1 className="projectTitle">{siteConfig.title}</h1>
-    </div>
-
-    <h2 style={{marginTop : "0.5em"}}>
-      一套可快速开发支持商业运营App的框架。
-    </h2>
-  </React.Fragment>
-);
-
-const PromoSection = props => (
-  <div className="section promoSection">
-    <div className="promoRow">
-      <div className="pluginRowBlock">{props.children}</div>
-    </div>
-  </div>
-);
 
 class HomeSplash extends React.Component {
   render() {
-    const language = this.props.language || "";
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Logo = props => (
+      <div className="projectLogo">
+        <img src={props.img_src} alt="Project Logo" />
+      </div>
+    );
+
+    const ProjectTitle = () => (
+      <h2 className="projectTitle">
+        {siteConfig.title}
+        <small>{siteConfig.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = props => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    class Button extends React.Component {
+      render() {
+        return (
+          <div className="pluginWrapper buttonWrapper">
+            <a className="button hero" href={this.props.href} target={this.props.target}>
+              {this.props.children}
+            </a>
+          </div>
+        );
+      }
+    }
+
+    Button.defaultProps = {
+      target: "_self"
+    };
+
     return (
       <SplashContainer>
+        <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
         <div className="inner">
-          <ProjectTitle />
+          <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
             <Button href={docUrl("introduction/getting-started", language)}>
               快速开始
@@ -85,114 +92,148 @@ class HomeSplash extends React.Component {
   }
 }
 
-const Installation = () => (
-  <div
-    className="productShowcaseSection"
-    style={{ textAlign: "center" }}
-  >
-    <h2 style={{marginTop : 10, marginBottom : 5}}>Installation</h2>
-    <MarkdownBlock>
-      ``` npm install --save
-      redux ```
-    </MarkdownBlock>
-  </div>
-);
-
-const Block = props => (
-  <Container
-    id={props.id}
-    background={props.background}
-    className={props.className}
-  >
-    <GridBlock align="center" contents={props.children} layout={props.layout}/>
-  </Container>
-);
-
-const FeaturesTop = props => (
-  <Block layout="fourColumn" className="rowContainer featureBlock">
-    {[
-      {
-        content: "定义统一规范的路由规则，支持按需加载，支持运营数据和异常数据的自动上报管理，配套的页面生命周期，让App的生命周期管理变得更加简单。",
-        //image: imgUrl('icon/time.png'),
-        image : imgUrl("noun_Check_1870817.svg"),
-        imageAlign: 'top',
-        title: "统一的路由管理"
-      },
-      {
-        content: "定义统一的网络层模式，以配置的方式来实现接口定义，支持纯客户端的Mock和缓存，洁面开发阶段彻底摆脱服务器端限制。",
-        image: imgUrl('cubes-solid.svg'),
-        imageAlign: 'top',
-        title: "完善的网络支持"
-      },
-      {
-        content: "清晰的层次和模块划分，三层数据隔离和共享机制的设计，让数据的隔离变得自然，共享变得简单。从设计上避免了数据管理混乱导致的Bug",
-        image: imgUrl('cogs-solid.svg'),
-        imageAlign: 'top',
-        title: "清晰的状态管理"
-      },
-      {
-        content: `框架统一处理调试需要的基础设施，各种类型的日志明确区分，让开发这可以更专心与业务Bug的调试`,
-        image: imgUrl('noun_debugging_1978252.svg'),
-        imageAlign: 'top',
-        title: "丰富的调试支持"
-      },
-    ]}
-  </Block>
-);
-
-const OtherLibraries = props => (
-  <Container className="rowContainer">
-    <h2 style={{margin : 0}}>
-      Other Libraries from the Redux Team
-    </h2>
-  <Block layout="fourColumn" className="libBlock">
-    {[
-      {
-        content: "Official React bindings for Redux",
-        title: "[React-Redux ![link2](img/external-link-square-alt-solid.svg)](https://react-redux.js.org) "
-      },
-      {
-        content: "A simple batteries-included toolset to make using Redux easier",
-        title: "[Redux Starter Kit ![link2](img/external-link-square-alt-solid.svg)](https://redux-starter-kit.js.org)"
-      },
-    ]}
-  </Block>
-  </Container>
-);
-
-const DocsSurvey = () => (
-  <Block layout="twoColumn" className="docsSurvey rowContainer">
-    {[
-      {
-        content: "We're planning a revamp of the Redux docs content.  Please help us make the docs better by **[filling out this survey](https://docs.google.com/forms/d/e/1FAIpQLSfzIkY3fXZ8PrQKScYMK0YoEgALfAK2qQ0mOj1_ibKv2qDTuQ/viewform)**, so we can determine what changes would help the most.  Thanks!",
-        title: "Help Us Improve the Redux Docs!"
-      },
-    ]}
-  </Block>
-)
-
 class Index extends React.Component {
   render() {
-    const language = this.props.language || "";
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
+    const Block = props => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const FeatureCallout = () => (
+      <div
+        className="productShowcaseSection paddingBottom"
+        style={{textAlign: 'center'}}>
+        <h2>Feature Callout</h2>
+        <MarkdownBlock>减少20%开发时间实现更可靠的App</MarkdownBlock>
+      </div>
+    );
+
+    const TryOut = () => (
+      <Block id="try">
+        {[
+          {
+            content:
+              'To make your landing page more attractive, use illustrations! Check out ' +
+              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
+              'The illustrations you see on this page are from unDraw.',
+            image: `${baseUrl}img/undraw_code_review.svg`,
+            imageAlign: 'left',
+            title: 'Wonderful SVG Illustrations',
+          },
+        ]}
+      </Block>
+    );
+
+    const Description = () => (
+      <Block background="dark">
+        {[
+          {
+            content:
+              'This is another description of how this project is useful',
+            image: `${baseUrl}img/undraw_note_list.svg`,
+            imageAlign: 'right',
+            title: 'Description',
+          },
+        ]}
+      </Block>
+    );
+
+    const LearnHow = () => (
+      <Block background="light">
+        {[
+          {
+            content:
+              'Each new Docusaurus project has **randomly-generated** theme colors.',
+            image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
+            imageAlign: 'right',
+            title: 'Randomly Generated Theme Colors',
+          },
+        ]}
+      </Block>
+    );
+
+    const Features = () => (
+      <Block layout="fourColumn" className="rowContainer featureBlock">
+        {[
+          {
+            content: "定义统一规范的路由规则，支持按需加载，支持运营数据和异常数据的自动上报管理，配套的页面生命周期，让App的生命周期管理变得更加简单。",
+            //image: imgUrl('icon/time.png'),
+            image : imgUrl("noun_Check_1870817.svg"),
+            imageAlign: 'top',
+            title: "统一的路由管理"
+          },
+          {
+            content: "定义统一的网络层模式，以配置的方式来实现接口定义，支持纯客户端的Mock和缓存，洁面开发阶段彻底摆脱服务器端限制。",
+            image: imgUrl('cubes-solid.svg'),
+            imageAlign: 'top',
+            title: "完善的网络支持"
+          },
+          {
+            content: "清晰的层次和模块划分，三层数据隔离和共享机制的设计，让数据的隔离变得自然，共享变得简单。从设计上避免了数据管理混乱导致的Bug",
+            image: imgUrl('cogs-solid.svg'),
+            imageAlign: 'top',
+            title: "清晰的状态管理"
+          },
+          {
+            content: `框架统一处理调试需要的基础设施，各种类型的日志明确区分，让开发这可以更专心与业务Bug的调试`,
+            image: imgUrl('noun_debugging_1978252.svg'),
+            imageAlign: 'top',
+            title: "丰富的调试支持"
+          },
+        ]}
+      </Block>
+    );
+
+    const Showcase = () => {
+      if ((siteConfig.users || []).length === 0) {
+        return null;
+      }
+
+      const showcase = siteConfig.users
+        .filter(user => user.pinned)
+        .map(user => (
+          <a href={user.infoLink} key={user.infoLink}>
+            <img src={user.image} alt={user.caption} title={user.caption} />
+          </a>
+        ));
+
+      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
+
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          <h2>Who is Using This?</h2>
+          <p>This project is used by all these people</p>
+          <div className="logos">{showcase}</div>
+          <div className="more-users">
+            <a className="button" href={pageUrl('users.html')}>
+              More {siteConfig.title} Users
+            </a>
+          </div>
+        </div>
+      );
+    };
 
     return (
       <div>
-        <HomeSplash language={language} />
+        <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
-
-          <div className="productShowcaseSection">
-            <Container background="light">
-              <FeaturesTop />
-            </Container>
-            <Container className="libsContainer" wrapper={false}>
-
-              {/*<OtherLibraries/>*/}
-              {/*<DocsSurvey />*/}
-            </Container>
-            <Container background="light">
-
-            </Container>
-          </div>
+          <Features />
+          {/*<FeatureCallout />*/}
+          {/*<LearnHow />*/}
+          {/*<TryOut />*/}
+          {/*<Description />*/}
+          {/*<Showcase />*/}
         </div>
       </div>
     );
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,51 +8,52 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-const siteConfig = {
-  title: "Trident", // Title for your website.
-  tagline: "一套可快速开发支持商业运营App的框架。",
-  url: "https://redux.js.org", // Your website URL
-  baseUrl: "", // Base URL for your project */
-  docsUrl : "", // don't prefix docs URLs with /docs/
-  // algolia: {
-  //   apiKey: '518c6e3c629811d8daa1d21dc8bcfa37',
-  //   indexName: 'redux',
-  //   algoliaOptions: {}
+// List of projects/orgs using your project for the users page.
+const users = [
+  // {
+  //   caption: 'User1',
+  //   // You will need to prepend the image path with your baseUrl
+  //   // if it is not '/', like: '/test-site/img/image.jpg'.
+  //   image: '/img/undraw_open_source.svg',
+  //   infoLink: 'https://www.facebook.com',
+  //   pinned: true,
   // },
+];
 
+const siteConfig = {
+  title: 'Trident', // Title for your website.
+  tagline: '一套可快速开发支持商业运营App的框架',
+  // url: 'https://your-docusaurus-test-site.com', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "Trident",
-  organizationName: "Webank App Team",
+  projectName: 'trident-website',
+  organizationName: 'WeBank App Team',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: "introduction/getting-started", label: "快速开始" },
-    { href: "http://git.weoa.com/app/trident", label: "Gitlab" },
-    // { doc : "api/api-reference", label : "API"},
-    // { doc : "faq", label : "FAQ"},
-    // { href : "https://www.github.com/reduxjs/redux", label : "Github"},
-    // { href: "/introduction/getting-started#help-and-discussion", label: "Need help?" },
+    {doc: 'getting-started', label: 'Docs'},
+    {blog: true, label: 'Blog'},
   ],
 
+  // If you have users set above, you add it here:
+  users,
+
   /* path to images for header/footer */
-  headerIcon: "img/trident-icon.png",
-  footerIcon: "img/trident-icon.png",
-  favicon: "img/favicon/favicon.ico",
+  // headerIcon: 'img/favicon.ico',
+  // footerIcon: 'img/favicon.ico',
+  // favicon: 'img/favicon.ico',
 
   /* Colors for website */
   colors: {
-    primaryColor: "#2653A1",
-    secondaryColor: "#D80D18",
-    accentColor1: "#717171",
-    accentColor2: "#F3EAFF",
-    accentColor3: "#D2B9F3",
-    accentColor4: "#ECF4F9",
-    accentColor5: "#CBDDEA",
-    accentColor6: "#2F5773"
+    primaryColor: '#503577',
+    secondaryColor: '#382553',
   },
 
   /* Custom fonts for website */
@@ -70,42 +71,34 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: 'Copyright (c) 2015-present 微众银行App团队',
+  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "monokai"
+    theme: 'default',
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: [
-    "/scripts/sidebarScroll.js",
-    "/scripts/codeblock.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
-    "https://buttons.github.io/buttons.js",
-  ],
-
-  // Add custom stylesheets here
-  stylesheets: ['/css/codeblock.css'],
-
-  // Last update time
-  enableUpdateTime: true,
+  scripts: ['https://buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
-  // onPageNav: "separate",
+  onPageNav: 'separate',
   // No .html extensions for paths.
   cleanUrl: true,
-  // Collapsible categories in the side navigation.
-  docsSideNavCollapsible: true,
 
   // Open Graph and Twitter card images.
-  ogImage: "img/redux-logo-landscape.png",
-  twitterImage: "img/redux-logo-twitter.png",
+  ogImage: 'img/undraw_online.svg',
+  twitterImage: 'img/undraw_tweetstorm.svg',
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  // repoUrl: "https://github.com/unpourtous/react-native-popup-stub",
-  // gaTrackingId : "UA-130598673-1",
+  //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
