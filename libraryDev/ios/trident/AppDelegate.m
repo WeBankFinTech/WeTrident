b7081c3ff4b444c596b18f1c8e160d981f34fe5d
Eric Hua <erichua@Erics-MacBook-Pro.local>
2019-08-21 20:14:19 +0800

    libraryDev support 0.51.0

--- libraryDev/ios/trident/AppDelegate.m
+++ libraryDev/ios/trident/AppDelegate.m
@@ -25,7 +25,7 @@
   #endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"soga"
+                                                      moduleName:@"libraryDev"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
