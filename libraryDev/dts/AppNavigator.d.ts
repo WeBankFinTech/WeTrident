046db72cea5eb6bb920dd6896c475bafbaeb1407
erichua <erichua@webank.com>
2019-10-29 21:36:25 +0800

    生成d.ts

--- libraryDev/dts/AppNavigator.d.ts
+++ libraryDev/dts/AppNavigator.d.ts
@@ -1,34 +1,24 @@
 declare module '@webank/trident' {
   interface AppNavigator {
-  example: {
-    BechcheckScene(): void 
-    DddScene(): void 
-    DemoScene(): void 
-    EeeeScene(): void 
-    FrameworkScene(): void 
-    KkkScene(): void 
-    NavAScene(): void 
-    NavBScene(): void 
-    NavCScene(): void 
-    NavigationScene(): void 
-    NetworkScene(): void 
-    PluginStoreScene(): void 
-    SadfasdfScene(): void 
-    SdssdfScene(): void 
-    SfdsdfaScene(): void 
-    StateManagementScene(): void 
-    StateShareScene(): void 
-    WeewweScene(): void 
-  },
-  tridentPluginWebView: {
-    WebViewScene(): void 
-  },
-  ui: {
-    AAAAScene(): void 
-    BbbbScene(): void 
-    CccccScene(): void 
-    UIScene(): void 
-  },
+    example: {
+      BechcheckScene(): void
+      DemoScene(): void
+      FrameworkScene(): void
+      NavAScene(): void
+      NavBScene(): void
+      NavCScene(): void
+      NavigationScene(): void
+      NetworkScene(): void
+      PluginStoreScene(): void
+      StateManagementScene(): void
+      StateShareScene(): void
+    },
+    tridentPluginWebView: {
+      WebViewScene(): void
+    },
+    ui: {
+      UIScene(): void
+    },
 
   }
 }
