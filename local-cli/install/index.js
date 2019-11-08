ee0099a23fa9ed0905e2aa1238cf6fc7d9806ed2
erichua23 <erichua23@gmail.com>
2019-11-08 15:20:36 +0800

    fix tdt install error

--- local-cli/install/index.js
+++ local-cli/install/index.js
@@ -1,5 +1,5 @@
 const execSync = require('../utils/execSync')
-const npmConfig = require('../npmConfig')
+const npmConfig = require('../config/npmConfig')
 const installAll = () => {
   // execSync('node ./node_modules/react-native/local-cli/cli.js bundle --platform android --entry-file index.js --bundle-output android/app/main/assets/main.jsbundle --assets-dest android/res/ --dev false')
   execSync(npmConfig.npm_install_all)
