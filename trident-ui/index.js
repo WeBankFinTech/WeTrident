f0fadd0393dea42ddac29d094ee01549fd68f5a7
lhtin <lehuading@qq.com>
2019-11-09 17:52:06 +0800

    add: export some components

--- trident-ui/index.js
+++ trident-ui/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button/Button'
-export { default as List } from './List'
-export * from './Layout/Layout'
+export { default as List } from './List/List'
+export {Row, Column} from './Layout/Layout'
+export {default as FinancialInput} from './Input/FinancialInput'
