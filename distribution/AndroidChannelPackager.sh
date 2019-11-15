1ca367f3d79eb7be6748226572351daa6a8d0bb6
williamdeng <williamdeng@webank.com>
2019-11-15 16:43:59 +0800

    android-distribution of channel packages

---
+++ distribution/AndroidChannelPackager.sh
@@ -0,0 +1,27 @@
+echo 【1/4】清理文件
+rm -rf bin
+mkdir bin
+
+echo 【2/4】准备源文件
+sourceApk=android_release.apk
+cp $sourceApk ./bin
+
+function buildApks()
+{
+
+	echo 【3/4】开始打包
+	#开始打CDN包....
+	java -jar $PWD/WeBankChannelTool $PWD/$sourceApk channel_cdn.txt
+	#开始打商店包....
+	java -jar $PWD/WeBankChannelTool $PWD/$sourceApk channel_store.txt
+
+	cp *.zip ./bin
+	rm *.zip
+
+	cd -
+    echo 【4/4】打包完成
+
+}
+
+buildApks
+
