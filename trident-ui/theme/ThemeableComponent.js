914a529db9cc09bf2c824e19ed9efa93b2fa96e4
erichua23 <erichua23@gmail.com>
2019-11-18 13:13:34 +0800

    add the deleted theme back

---
+++ trident-ui/theme/ThemeableComponent.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+import PropTypes from 'prop-types'
+
+export default class ThemeableComponent extends Component {
+  static contextTypes = {
+    theme: PropTypes.object.isRequired
+  }
+
+  getComponentTheme () {
+    const theme = _.get(this.context, 'theme')
+    if (this.namespace && _.isObject(theme[this.namespace])) {
+      return this._mergeTheme(theme[this.namespace])
+    }
+    return this.props
+  }
+
+  _mergeTheme (customComponentTheme = {}) {
+    return {
+      ...customComponentTheme,
+      ...(this.props || {}),
+    }
+  }
+
+  render () {
+    return null
+  }
+}
