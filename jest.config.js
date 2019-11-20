74f40ddeaad35c8733ed0fc8de9754e82716d638
erichua23 <erichua23@gmail.com>
2019-11-20 20:03:51 +0800

    add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,10 @@
 // const path = require('path')
 module.exports = {
-  notify: true,
+  // notify: true,
   verbose: true,
-  // // preset: 'react-native-babel-preset',
-  // bail: true, // stop @ the first fail
-  //
-  // collectCoverage: false,
-  // coverageDirectory: './__tests__/__coverage__/',
-  // coverageReporters: ['lcov'],
-  //
-  // // 将需要测试的加入到这里面来
-  // // testMatch: [
-  // //   '**/__tests__/services/ProdInfo-test.js',
-  // //   '**/__tests__/services/IncomeService-test.js',
-  // //   '**/__tests__/utils/DateUtils-test.js'
-  // // ],
-  // modulePathIgnorePatterns: ['example/node_modules/', 'website/', 'lib/'],
-  // moduleNameMapper: {
-  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__tests__/__mocks__/imageMock.js'
-  // },
-  // moduleDirectories: [
-  //   'node_modules',
-  //   'src'
-  // ],
-  // transformIgnorePatterns: [ // Default: ["/node_modules/"]
-  //   // 'node_modules/(?!react-native|@unpourtous|react|lottie-react-native)'
-  //   '!node_modules/'
-  // ],
-  // transform: {
-  //   '^.+\\.js$': 'babel-jest'
-  // },
-  // setupFilesAfterEnv: ['./__tests__/setupJest.js']
+  // Since axios has both xhr and node network adapter,
+  // xhr adapter has cross domain problem when use jest test framework, change this to node to avoid the cross domain problem
+  testEnvironment: "node",
+
+  setupFilesAfterEnv: ['./jest.setup.js']
 }
