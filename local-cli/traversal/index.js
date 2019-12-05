22c4622e4e53ed6b1617061048312cdac991bca1
williamdeng <williamdeng@webank.com>
2019-12-05 16:52:05 +0800

    fix-tdt traversal start

--- local-cli/traversal/index.js
+++ local-cli/traversal/index.js
@@ -3,8 +3,8 @@ const shell = require('shelljs')
 function start (root) {
   let path = shell.exec('find . -name start-server.sh').stdout
   if (path) {
-    let dictionary = path.replace('/start-server.sh', '')
-    shell.exec(`cd ${dictionary}/../../`)
+    let dictionary = path.replace('/start-server.sh', '/../../')
+    shell.exec(`cd ${dictionary}`)
     shell.exec('sh ./traversal-server/scripts/start-server.sh')
   } else {
     shell.echo('Error: Can not find start scripts, check your environment and config please.');
