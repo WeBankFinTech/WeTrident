bdddadc25c7e0e712b3a63091347923d484e5656
williamdeng <williamdeng@webank.com>
2019-12-06 20:10:06 +0800

    traversal-测试结果优化错误信息展示

--- traversal-server/routes/analysis.js
+++ traversal-server/routes/analysis.js
@@ -7,19 +7,16 @@ router.get('/', function(req, res, next) {
 	let title = `[${fileName.replace('.txt', '')}] Traversal Results`
 	let traversingRecords
 	let finishedRecords
-	let errorRecords
 	if (fileName) {
 		loadRecords(fileName).then(
 			result => {
 				if (result) {
 					traversingRecords = parseTraversingRecords(result.traversingRecords)
 					finishedRecords = parseFinishedRecords(result.finishedRecords)
-					errorRecords = parseErrors(result.errors)
 					res.render('analysis', {
 						title,
 						traversingRecords,
-						finishedRecords,
-						errorRecords
+						finishedRecords
 					})
 				} else {
 					res.render('analysis', {
@@ -46,20 +43,51 @@ router.get('/', function(req, res, next) {
 function parseTraversingRecords(sourceData) {
 	let targetData
 	for (let index = 0; index < sourceData.length; index++) {
-		let record = sourceData[index].trim().replace('[traversal]', '').split('_')
-		if (!targetData) {
-			targetData = {}
+		if (sourceData[index].trim().startWith('\\[traversal\\]')) {
+			let record = sourceData[index].trim().replace('[traversal]', '').split('_')
+
+			if (!targetData) {
+				targetData = {}
+			}
+
+			if (record.length !== 3) {
+				continue
+			}
+
+			if (!targetData[record[0]]) {
+				targetData[record[0]] = {}
+			}
+			if (!targetData[record[0]][record[1]]) {
+				targetData[record[0]][record[1]] = {}
+			}
+			if (!targetData[record[0]][record[1]][record[2]]) {
+				targetData[record[0]][record[1]][record[2]] = []
+			}
+
+		} else if (sourceData[index].trim().startWith('\\[error\\]') && index > 0 && sourceData[index - 1].trim().startWith('\\[traversal\\]')) {
+			let t_record = sourceData[index - 1].trim().replace('[traversal]', '').split('_')
+			let e_record = sourceData[index].trim().replace('[error]', '').split('_')
+
+			if (!targetData) {
+				targetData = {}
+			}
+
+			if (t_record[0] !== e_record[0] || t_record[1] !== e_record[1]) {
+				continue
+			}
+
+			if (!targetData[t_record[0]]) {
+				targetData[t_record[0]] = {}
+			}
+			if (!targetData[t_record[0]][t_record[1]]) {
+				targetData[t_record[0]][t_record[1]] = {}
+			}
+			if (!targetData[t_record[0]][t_record[1]][t_record[2]]) {
+				targetData[t_record[0]][t_record[1]][t_record[2]] = []
+			}
+
+			targetData[t_record[0]][t_record[1]][t_record[2]].push(e_record[2])
 		}
-		if (record.length !== 3) {
-			continue
-		}
-		if (!targetData[record[0]]) {
-			targetData[record[0]] = {}
-		}
-		if (!targetData[record[0]][record[1]]) {
-			targetData[record[0]][record[1]] = []
-		}
-		targetData[record[0]][record[1]].push(record[2])
 	}
 	return targetData
 }
@@ -82,27 +110,6 @@ function parseFinishedRecords(sourceData) {
 	return targetData
 }
 
-function parseErrors(sourceData) {
-	let targetData
-	for (let index = 0; index < sourceData.length; index++) {
-		let record = sourceData[index].trim().replace('[error]', '').split('_')
-		if (!targetData) {
-			targetData = {}
-		}
-		if (record.length !== 3) {
-			continue
-		}
-		if (!targetData[record[0]]) {
-			targetData[record[0]] = {}
-		}
-		if (!targetData[record[0]][record[1]]) {
-			targetData[record[0]][record[1]] = []
-		}
-		targetData[record[0]][record[1]].push(record[2])
-	}
-	return targetData
-}
-
 function loadRecords(fileName) {
 	let fs = require('fs')
 	return new Promise((resolve, reject) => {
@@ -113,13 +120,11 @@ function loadRecords(fileName) {
 						console.log(error)
 						reject()
 					} else {
-						let traversingRecords = data.toString().trim().split('\n').filter(item => !!item && item.trim().startWith('\\[traversal\\]'))
+						let traversingRecords = data.toString().trim().split('\n').filter(item => !!item && (item.trim().startWith('\\[traversal\\]') || item.trim().startWith('\\[error\\]')))
 						let finishedRecords = data.toString().trim().split('\n').filter(item => !!item && item.trim().startWith('\\[finished\\]'))
-						let errors = data.toString().trim().split('\n').filter(item => !!item && item.trim().startWith('\\[error\\]'))
 						resolve({
 							traversingRecords,
-							finishedRecords,
-							errors
+							finishedRecords
 						})
 					}
 				})
