2513fd568cac2b5e97538c5181d857a6aa159757
shuqiangwu <shuqiangwu@webank.com>
2019-12-07 20:50:33 +0800

    fix seed dyModules

--- app-seed/src/index.js
+++ app-seed/src/index.js
@@ -3,6 +3,7 @@
  */
 import React, { Component } from 'react'
 import { TridentApp } from '@webank/trident'
+import RNEnv from '@webank/trident/library/utils/RNEnv'
 
 export default class AppEntry extends Component {
   render () {
@@ -10,7 +11,10 @@ export default class AppEntry extends Component {
     return (
       <TridentApp
         container={require('./container').default}
-        modules={require('./modules').default} />
+        modules={require('./modules').default}
+        dyModules={require('./modules').dyModules}
+        showWTConsole={!RNEnv.isRemoteDebug() && RNEnv.isDev()}
+      />
     )
   }
 }
