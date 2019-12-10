d5c17412993c75947ff240b084fa2d3465b3e785
erichua23 <erichua23@gmail.com>
2019-12-10 22:14:27 +0800

    modify app-seed index

--- app-seed/src/index.js
+++ app-seed/src/index.js
@@ -2,13 +2,38 @@
  * Created by erichua on 26/12/2017.
  */
 import React, { Component } from 'react'
-import { TridentApp, RNEnv } from '@webank/trident'
+import { AppNavigator, TridentApp, NavBar, RNEnv } from '@webank/trident'
+
+import CardStackStyleInterpolator from '@unpourtous/react-navigation/src/views/CardStack/CardStackStyleInterpolator'
+
+import { LightTheme, createTheme } from '@webank/trident'
+
+const weuiTheme = createTheme(LightTheme)
 
 export default class AppEntry extends Component {
   render () {
     // All you need to setup
     return (
       <TridentApp
+        theme={weuiTheme}
+        navigationConfig={{
+          navigationOptions: {
+            gesturesEnabled: true,
+            header: ({ scene, getScreenDetails, navigation: { state: { params = {} } } }) => {
+              const sceneDetail = getScreenDetails(scene)
+              return <NavBar title={sceneDetail.options.headerTitle} onPressLeft={() => {
+                AppNavigator.goBack()
+              }} />
+            }
+          },
+          headerMode: 'screen',
+          // make android and ios have the same transition effect
+          transitionConfig: () => ({
+            screenInterpolator: sceneProps => {
+              return CardStackStyleInterpolator.forHorizontal(sceneProps)
+            }
+          })
+        }}
         container={require('./container').default}
         modules={require('./modules').default}
         dyModules={require('./modules').dyModules}
