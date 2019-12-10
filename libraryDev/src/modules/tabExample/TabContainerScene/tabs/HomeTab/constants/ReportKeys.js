b24f6ba9784f5cc779d32e877855862c0c77b86f
erichua23 <erichua23@gmail.com>
2019-12-10 13:28:00 +0800

    remove unused modules

--- libraryDev/src/modules/tabExample/TabContainerScene/tabs/HomeTab/constants/ReportKeys.js
+++
@@ -1,9 +0,0 @@
-/**
- * 每个Scene的上报KEY分别放在Scene的constants下面
- * 上报全都走ReportKey常量，并且请标明上报的用途和key的含义
- *
- * Created by rcrabwu on 2019-10-24T08:41:28.998Z.
- */
-export default Object.freeze({
-
-})
--- libraryDev/src/modules/tabExample/manifest.js
+++
@@ -1,9 +0,0 @@
-/**
- * 记录所有Scene的列表
- *
- * Created by rcrabwu on 2019-10-24T08:40:34.034Z.
- */
-
-export default [
-  require("./TabContainerScene").default
-]
