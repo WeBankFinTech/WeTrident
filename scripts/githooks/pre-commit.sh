a2c325dd83c9c8e112f526e4e531e157e3cb6429
erichua23 <erichua23@gmail.com>
2019-12-17 16:54:20 +0800

    add hushy git hook to force code style

---
+++ scripts/githooks/pre-commit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Ensure all JavaScript files staged for commit pass standard code style
+function xargs-r() {
+  # Portable version of "xargs -r". The -r flag is a GNU extension that
+  # prevents xargs from running if there are no input files.
+  if IFS= read -r -d $'\n' path; then
+    { echo "$path"; cat; } | xargs $@
+  fi
+}
+
+git diff --name-only --cached --relative | grep '\.jsx\?$' | sed 's/[^[:alnum:]]/\\&/g' | xargs-r -E '' -t node_modules/standard/bin/cmd.js ./**/*.js | node_modules/snazzy/bin/cmd.js
+
+if [[ $? -ne 0 ]]; then
+  echo 'JavaScript Standard Style errors were detected. Aborting commit.'
+  exit 1
+fi
